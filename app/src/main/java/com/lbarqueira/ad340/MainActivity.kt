ea5d14b94056ac9b434867f8f346bd33014de56e
Luis Barqueira <65919567+lbarqueira@users.noreply.github.com>
2021-06-16 17:27:57 +0100

    Week 2 - Activities, Lifecycles, Interactive UI

--- app/src/main/java/com/lbarqueira/ad340/MainActivity.kt
+++ app/src/main/java/com/lbarqueira/ad340/MainActivity.kt
@@ -2,10 +2,61 @@ package com.lbarqueira.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // region Setup methods
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            // Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+
+            Log.i("MainActivity", "Button Clicked")
+            val zipcode: String = zipcodeEditText.text.toString()
+
+            if (zipcode.length != 6) {
+                Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    // endregion Setup methods
+
+    // region Teardown methods
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // endregion Teardown methods
 }
